2c9ea492caf2b30c2fd5091d34048075456140c6
Malek Mousa <malek.mousa1407@gmail.com>
2021-08-11 20:30:11 +0400

    Configured User Authentication

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -2,9 +2,13 @@ const express = require('express');
 // We only need the routing methods from express
 const router = express.Router();
 const bcryptjs = require('bcryptjs');
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const jwtSecret = process.env.JWT_SECRET;
 const UserModel = require('../models/UserModel.js');
 const cloudinary = require('cloudinary').v2;
 
+// Get all users from DB in JSON format
 router.get('/',
     (req, res) => {
         UserModel.find().then((dbDocument) => {
@@ -15,8 +19,11 @@ router.get('/',
     }
 );
 
+// Create a User
 router.post('/create',
-    (req, res) => { 
+    (req, res) => {
+
+        // Contains the user's submission
         const formData = {
             firstName: req.body.firstName,
             lastName: req.body.lastName,
@@ -24,54 +31,127 @@ router.post('/create',
             password: req.body.password,
             phoneNumber: req.body.phoneNumber
         }
-        // Check for email
+
         UserModel
-        .findOne({ email: formData.email })
-        .then(async(dbDocument) => {
-                // Check if email exists
-            if (dbDocument){
-                res.send('Sorry, an account already exists :(');
-                // If email is unique, create account
-            } else{
-                //If avatar is included, upload to cloudinary
-                if(Object.values(req.files).length > 0){
-                    const files = Object.values(req.files);
-                    // upload to Cloudinary
-                    await cloudinary.uploader.upload(
-                        files[0].path,
-                        (cloudinaryErr, cloudinaryResult) => {
-                            if(cloudinaryErr){
-                                console.log(cloudinaryErr);
-                            }else{
-                             // Include the image url in formData   
-                              formData.avatar = cloudinaryResult.url;                                                                               
+            .findOne({ email: formData.email }) // for example: jondoe@gmail.com
+            .then(
+                async (dbDocument) => {
+                    // If email exists, reject request
+                    if (dbDocument) {
+                        res.send("Sorry, an account with this email already exists.");
+                    }
+                    // Otherwise, create the account
+                    else {
+                        // If avatar file is included...
+                        if (Object.values(req.files).length > 0) {
+                            const files = Object.values(req.files);
+                            // upload to Cloudinary
+                            await cloudinary.uploader.upload(
+                                files[0].path,
+                                (cloudinaryErr, cloudinaryResult) => {
+                                    if (cloudinaryErr) {
+                                        console.log(cloudinaryErr);
+                                    } else {
+                                        // Include the image url in formData
+                                        formData.avatar = cloudinaryResult.url;
+                                    }
+                                }
+                            )
+                        };
+
+                        // Generate a Salt
+                        bcryptjs.genSalt(
+                            (err, theSalt) => {
+                                // Combine user's password + Salt to hash the password
+                                bcryptjs.hash(
+                                    formData.password,  // first ingredient
+                                    theSalt,    // second ingredient
+                                    (err, hashedPassword) => {
+                                        // Replace the password in the form with the hash
+                                        formData.password = hashedPassword;
+                                        // Create the document
+                                        // Use the UserModel to create a new document
+                                        UserModel
+                                            .create(formData)
+                                            .then(
+                                                (dbDocument) => {
+                                                    res.send(dbDocument);
+                                                }
+                                            )
+                                            .catch(
+                                                (error) => {
+                                                    console.log(error);
+                                                }
+                                            );
+                                    }
+                                );
                             }
-                        }
-                    )
-                }                
-                
-                // Generate a salt
-                bcryptjs.genSalt(
-                    (err, theSalt) => {
-                    // Combine the user's password + Salt to hash the password
-                    bcryptjs.hash(
-                        formData.password,
-                        theSalt,
-                        (err, hashedPassword) => {
-                            // Replace the password in the form with the hash
-                            formData.password = hashedPassword;
-                            // Create a new user account with hashed password
-                            UserModel
-                            .create(formData)
-                            .then(dbDocument => res.json(dbDocument))
-                            .catch(error => console.log(error));
-                        }
-                    )
-                });              
-            }
-        }).catch((err) =>{
-            console.log(err);   
-        });
+                        );
+                    }
+                }
+            )
+            .catch(
+                (err) => {
+                    console.log(err);
+                }
+            )
+    }
+);
+
+// Login a User
+router.post('/login',
+    (req, res) => {
+        // Capture form data
+        const formData = {
+            email: req.body.email,
+            password: req.body.password,
+        }
+        // Check if email exists
+        UserModel
+            .findOne({ email: formData.email })
+            .then(
+                (dbDocument) => {
+                    // If email exists
+                    if (dbDocument) {
+                        // Compare the password sent againt password in database
+                        bcryptjs.compare(
+                            formData.password,          // password user sent
+                            dbDocument.password         // password in database
+                        )
+                        .then(
+                            (isMatch) => {
+                                // If passwords match...
+                                if (isMatch) {
+                                    // Generate the Payload
+                                    const payload = {
+                                        _id: dbDocument._id,
+                                        email: dbDocument.email
+                                    }
+                                    // Generate the jsonwebtoken
+                                    jwt
+                                    .sign(
+                                        payload,
+                                        jwtSecret,
+                                        (err, jsonwebtoken) => {
+                                            if (err) console.log(err);                                                
+                                            else res.send(jsonwebtoken);                                                                                            
+                                        });
+                                }// If passwords don't match, reject login
+                                    else {
+                                        res.send("Wrong email or password");
+                                    }
+                                }
+                            )
+                            .catch(err => console.log(err));
+                    }
+                    // If email does not exist
+                    else {
+                        // reject the login
+                        res.send("Wrong email or password");
+                    }
+                }
+            )
+            .catch(err => console.log(err));
     }
 );
 
--- server.js
+++ server.js
@@ -14,6 +14,50 @@ const expressFormData = require('express-form-data');
 // Import application routes 
 const userRoutes = require('./routes/user-routes');
 const productRoutes = require('./routes/product-routes.js');
+// Passport and Passport-JWT for user authetication
+const passport = require('passport');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const jwtSecret = process.env.JWT_SECRET;
+
+// This will tell passport where to find the JWT
+// and how to extract the payload
+const passportJwtOptions = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: jwtSecret,
+}
+
+// This function will tell passport how what to do
+// with the payload.
+const passportJwt = (passport) => {
+    passport.use(
+        new JwtStrategy(
+            passportJwtOptions,
+            (jwtPayload, done) => {
+                // Tell passport what to do with payload
+                UserModel
+                .findOne({ _id: jwtPayload._id })
+                .then(
+                    (dbDocument) => {
+                        // The done() function will pass the 
+                        // dbDocument to Express. The user's 
+                        // document can then be access via req.user
+                        return done(null, dbDocument)
+                    }
+                )
+                .catch(
+                    (err) => {
+                        // If the _id or anything is invalid,
+                        // pass 'null' to Express.
+                        if(err) console.log(err);                        
+                        return done(null, null)
+                    }
+                )
+            }
+        )
+    )
+};
+passportJwt(passport);
 
 // Middleware
 // Parse urlencoded bodies and where the Content-Type header matches the type option
@@ -26,6 +70,7 @@ server.use(cors());
 server.use(expressFormData.parse());
 
 const mongoose = require('mongoose');
+const UserModel = require('./models/UserModel');
 const connectionString = process.env.MONGODB_CONNECTION_STRING;
 const connectionConfig = {
     useNewUrlParser: true,
